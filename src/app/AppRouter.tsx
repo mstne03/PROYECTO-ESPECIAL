7c38821ae51bc6f05fe127957624b3d5f3e90728
mstne03 <mserraitorne@gmail.com>
2025-10-06 12:24:18 +0200

    Hacer basename dinámico para desarrollo y producción

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'
 
 const AppRouter: React.FC = () => {
+  // Usar basename solo en producción (GitHub Pages)
+  const basename = import.meta.env.PROD ? '/PROYECTO-ESPECIAL' : '/'
+  
   return (
-    <Router basename="/PROYECTO-ESPECIAL">
+    <Router basename={basename}>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* Aquí puedes agregar más rutas en el futuro */}
